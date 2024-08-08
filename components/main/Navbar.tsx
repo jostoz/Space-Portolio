c3b171b675d5fbd7694a30c955870982f2a369e5
Tu Nombre <jostoztado@gmail.com>
2024-08-08 02:55:53 -0600

    nav bar

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
         <a
-          href="#about-me"
+          href="#home"
           className="h-auto w-auto flex flex-row items-center"
         >
           <Image
@@ -19,20 +19,26 @@ const Navbar = () => {
           />
 
           <span className="font-bold ml-[10px] hidden md:block text-gray-300">
-            Josuetoz
+            Fer Unda
           </span>
         </a>
 
         <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
           <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
-            <a href="#about-me" className="cursor-pointer">
-              About me
+            <a href="#home" className="cursor-pointer">
+              Home
             </a>
-            <a href="#skills" className="cursor-pointer">
-              Skills
+            <a href="#studio" className="cursor-pointer">
+              Studio
             </a>
-            <a href="#projects" className="cursor-pointer">
-              Projects
+            <a href="#work" className="cursor-pointer">
+              Work
+            </a>
+            <a href="#about" className="cursor-pointer">
+              About
+            </a>
+            <a href="#contact" className="cursor-pointer">
+              Contact
             </a>
           </div>
         </div>
