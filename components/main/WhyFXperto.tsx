a580a61b1b6add779ce248f10d8f23ae35149538
jostoz <jostoztado@gmail.com>
2025-03-19 21:07:05 -0600

    Add slideInFromTop animation to WhyFXperto component for enhanced visual effects

--- components/main/WhyFXperto.tsx
+++ components/main/WhyFXperto.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-import { slideInFromLeft, slideInFromRight } from "@/utils/motion";
+import { slideInFromLeft, slideInFromRight, slideInFromTop } from "@/utils/motion";
 import { ChartBarIcon, ShieldCheckIcon, UserGroupIcon, CurrencyDollarIcon } from "@heroicons/react/24/outline";
 
 const WhyFXperto = () => {
