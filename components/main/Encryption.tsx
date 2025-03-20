5f403cc837ec2c8ae3bf9e6561f8c94212475405
jostoz <jostoztado@gmail.com>
2025-03-19 18:51:19 -0600

    Refactor Encryption component layout and content, updating animations and adding new images for enhanced visual appeal

--- components/main/Encryption.tsx
+++ components/main/Encryption.tsx
@@ -1,73 +1,65 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-import { slideInFromLeft, slideInFromRight, slideInFromTop } from "@/utils/motion";
-import { SparklesIcon } from "@heroicons/react/24/solid";
+import { slideInFromTop } from "@/utils/motion";
+import Image from "next/image";
 
 const Encryption = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen relative w-full h-full">
-      <div className="absolute w-full h-auto flex flex-col items-center justify-center z-10">
+    <div className="flex flex-row relative items-center justify-center min-h-screen w-full h-full">
+      <div className="absolute w-auto h-auto top-0 z-[5]">
         <motion.div
           variants={slideInFromTop}
-          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+          className="text-[40px] font-medium text-center text-gray-200"
         >
-          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-          <h1 className="Welcome-text text-[13px]">
-            Ventajas de FXperto
-          </h1>
-        </motion.div>
-
-        <motion.div
-          variants={slideInFromLeft(0.5)}
-          className="text-[50px] text-white font-medium mt-[10px] text-center mb-[15px]"
-        >
-          ¿Por qué elegir FXperto?
-        </motion.div>
-
-        <motion.div
-          variants={slideInFromRight(0.5)}
-          className="cursive text-[20px] text-gray-200 mb-10 mt-[10px] text-center"
-        >
-          La plataforma preferida por profesionales financieros
+          Seguridad
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+            {" "}
+            &{" "}
+          </span>
+          Confianza
         </motion.div>
       </div>
 
-      <div className="flex flex-col items-center justify-center translate-y-[-50px] absolute z-20 w-auto h-auto">
+      <div className="flex flex-col items-center justify-center translate-y-[-50px] absolute z-[20] w-auto h-auto">
         <div className="flex flex-col items-center group cursor-pointer w-auto h-auto">
-          <div className="Welcome-box py-[15px] px-[47px] my-[20px] cursor-pointer border border-[#7042f88b] opacity-[0.9]">
-            <h1 className="Welcome-text text-[15px]">
-              Ventaja Competitiva
-            </h1>
-          </div>
-          <div className="Welcome-box py-[15px] px-[47px] my-[20px] cursor-pointer border border-[#7042f88b] opacity-[0.9]">
-            <h1 className="Welcome-text text-[15px]">
-              Decisiones Basadas en Datos
-            </h1>
-          </div>
-          <div className="Welcome-box py-[15px] px-[47px] my-[20px] cursor-pointer border border-[#7042f88b] opacity-[0.9]">
-            <h1 className="Welcome-text text-[15px]">
-              Transparencia Total
-            </h1>
-          </div>
-          <div className="Welcome-box py-[15px] px-[47px] my-[20px] cursor-pointer border border-[#7042f88b] opacity-[0.9]">
-            <h1 className="Welcome-text text-[15px]">
-              Innovación Continua
-            </h1>
-          </div>
+          <Image
+            src="/LockTop.png"
+            alt="Lock top"
+            width={50}
+            height={50}
+            className="w-[50px] translate-y-5 transition-all duration-200 group-hover:translate-y-11"
+          />
+          <Image
+            src="/LockMain.png"
+            alt="Lock Main"
+            width={70}
+            height={70}
+            className=" z-10"
+          />
+        </div>
+
+        <div className="Welcome-box px-[15px] py-[4px] z-[20] brder my-[20px] border-[#7042f88b] opacity-[0.9]">
+          <h1 className="Welcome-text text-[12px]">Protección de Datos</h1>
         </div>
       </div>
 
-      <div className="absolute z-10 w-full h-full flex justify-center items-center">
+      <div className="absolute z-[20] bottom-[10px] px-[5px]">
+        <div className="cursive text-[20px] font-medium text-center text-gray-300">
+          Sus operaciones protegidas con encriptación de extremo a extremo
+        </div>
+      </div>
+
+      <div className="w-full flex items-start justify-center absolute">
         <video
           loop
           muted
           autoPlay
           playsInline
-          className="w-full h-full object-cover opacity-[0.4]"
-        >
-          <source src="/encryption.webm" type="video/webm" />
-        </video>
+          preload="false"
+          className="w-full h-auto"
+          src="/encryption.webm/"
+        />
       </div>
     </div>
   );
